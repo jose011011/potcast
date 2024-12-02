9d108bf69cf42eaf9818cc47cac915a4e18b1fbf
jose011011 <juanmamani121432@gmail.com>
2024-12-02 12:14:53 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('¡Gracias por tu mensaje! Nos pondremos en contacto pronto.');
+});
